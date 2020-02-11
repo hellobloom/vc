af5e294b50958351bc7b1555cc9c484994fce66e
Jace <jacehensley@gmail.com>
2020-02-10 18:03:11 -0700

    Always return true for the playground

--- packages/verify-kit/src/validate/v1/structure/presentation.ts
+++ packages/verify-kit/src/validate/v1/structure/presentation.ts
@@ -11,12 +11,12 @@ import {
   BaseVCRevocationV1,
   ValidateFn,
 } from '@bloomprotocol/attestations-common'
-import {keyUtils} from '@transmute/es256k-jws-ts'
-import {EcdsaSecp256k1KeyClass2019, EcdsaSecp256k1Signature2019} from '@transmute/lds-ecdsa-secp256k1-2019'
-import EthWallet from 'ethereumjs-wallet'
+// import {keyUtils} from '@transmute/es256k-jws-ts'
+// import {EcdsaSecp256k1KeyClass2019, EcdsaSecp256k1Signature2019} from '@transmute/lds-ecdsa-secp256k1-2019'
+// import EthWallet from 'ethereumjs-wallet'
 
-const jsigs = require('jsonld-signatures')
-const {AuthenticationProofPurpose, AssertionProofPurpose} = jsigs.purposes
+// const jsigs = require('jsonld-signatures')
+// const {AuthenticationProofPurpose, AssertionProofPurpose} = jsigs.purposes
 
 const stripOwnerFromDID = (value: string) => value.substr(0, value.length - 6)
 
@@ -51,35 +51,37 @@ const validateCredentialProof = genValidateFn<AtomicVCProofV1>({
   jws: Utils.isNotEmptyString,
 })
 
-const isCredentialProofValid = async (value: any, data: any) => {
-  try {
-    const {didDocument} = await EthUtils.resolveDID(stripOwnerFromDID(value.verificationMethod))
-    const publicKey = didDocument.publicKey[0]
-    // TODO: Recover public key from JWS
-    const issuerPublicKey = EthWallet.fromPrivateKey(
-      Buffer.from('efca4cdd31923b50f4214af5d2ae10e7ac45a5019e9431cc195482d707485378', 'hex'),
-    ).getPublicKeyString()
+const isCredentialProofValid = async (_: any, __: any) => {
+  return true
 
-    const publicKeyJwk = await keyUtils.publicJWKFromPublicKeyHex(issuerPublicKey)
+  // try {
+  //   const {didDocument} = await EthUtils.resolveDID(stripOwnerFromDID(value.verificationMethod))
+  //   const publicKey = didDocument.publicKey[0]
+  //   // TODO: Recover public key from JWS
+  //   const issuerPublicKey = EthWallet.fromPrivateKey(
+  //     Buffer.from('efca4cdd31923b50f4214af5d2ae10e7ac45a5019e9431cc195482d707485378', 'hex'),
+  //   ).getPublicKeyString()
 
-    const res = await jsigs.verify(data, {
-      suite: new EcdsaSecp256k1Signature2019({
-        key: new EcdsaSecp256k1KeyClass2019({
-          id: publicKey.id,
-          controller: publicKey.controller,
-          publicKeyJwk: publicKeyJwk,
-        }),
-      }),
-      compactProof: false,
-      documentLoader: EthUtils.documentLoader,
-      purpose: new AssertionProofPurpose(),
-      expansionMap: false, // TODO: remove this
-    })
+  //   const publicKeyJwk = await keyUtils.publicJWKFromPublicKeyHex(issuerPublicKey)
 
-    return res.verified === true
-  } catch {
-    return false
-  }
+  //   const res = await jsigs.verify(data, {
+  //     suite: new EcdsaSecp256k1Signature2019({
+  //       key: new EcdsaSecp256k1KeyClass2019({
+  //         id: publicKey.id,
+  //         controller: publicKey.controller,
+  //         publicKeyJwk: publicKeyJwk,
+  //       }),
+  //     }),
+  //     compactProof: false,
+  //     documentLoader: EthUtils.documentLoader,
+  //     purpose: new AssertionProofPurpose(),
+  //     expansionMap: false, // TODO: remove this
+  //   })
+
+  //   return res.verified === true
+  // } catch {
+  //   return false
+  // }
 }
 
 export const validateVerifiableCredential = genAsyncValidateFn<AtomicVCV1>({
@@ -107,39 +109,41 @@ const validateProof = genValidateFn<VPProofV1>({
   jws: Utils.isNotEmptyString,
 })
 
-const isPresentationProofValid = async (value: any, data: any) => {
-  try {
-    const {didDocument} = await EthUtils.resolveDID(stripOwnerFromDID(value.verificationMethod))
-    const publicKey = didDocument.publicKey[0]
+const isPresentationProofValid = async (_: any, __: any) => {
+  return true
 
-    // TODO: Recover public key from JWS
-    const holderPublicKey = EthWallet.fromPrivateKey(
-      Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex'),
-    ).getPublicKeyString()
+  // try {
+  //   const {didDocument} = await EthUtils.resolveDID(stripOwnerFromDID(value.verificationMethod))
+  //   const publicKey = didDocument.publicKey[0]
 
-    const publicKeyJwk = await keyUtils.publicJWKFromPublicKeyHex(holderPublicKey)
+  //   // TODO: Recover public key from JWS
+  //   const holderPublicKey = EthWallet.fromPrivateKey(
+  //     Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex'),
+  //   ).getPublicKeyString()
 
-    const res = await jsigs.verify(data, {
-      suite: new EcdsaSecp256k1Signature2019({
-        key: new EcdsaSecp256k1KeyClass2019({
-          id: publicKey.id,
-          controller: publicKey.controller,
-          publicKeyJwk: publicKeyJwk,
-        }),
-      }),
-      documentLoader: EthUtils.documentLoader,
-      purpose: new AuthenticationProofPurpose({
-        challenge: data.proof.challenge,
-        domain: data.proof.domain,
-      }),
-      compactProof: false,
-      expansionMap: false, // TODO: remove this
-    })
+  //   const publicKeyJwk = await keyUtils.publicJWKFromPublicKeyHex(holderPublicKey)
 
-    return res.verified === true
-  } catch {
-    return false
-  }
+  //   const res = await jsigs.verify(data, {
+  //     suite: new EcdsaSecp256k1Signature2019({
+  //       key: new EcdsaSecp256k1KeyClass2019({
+  //         id: publicKey.id,
+  //         controller: publicKey.controller,
+  //         publicKeyJwk: publicKeyJwk,
+  //       }),
+  //     }),
+  //     documentLoader: EthUtils.documentLoader,
+  //     purpose: new AuthenticationProofPurpose({
+  //       challenge: data.proof.challenge,
+  //       domain: data.proof.domain,
+  //     }),
+  //     compactProof: false,
+  //     expansionMap: false, // TODO: remove this
+  //   })
+
+  //   return res.verified === true
+  // } catch {
+  //   return false
+  // }
 }
 
 export const validateVerifiablePresentationV1 = genAsyncValidateFn<VPV1<AtomicVCV1>>({
