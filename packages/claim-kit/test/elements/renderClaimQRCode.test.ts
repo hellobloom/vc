6a1e7a53d81eb95437cf2a1ffa60e84e7636653a
Jace <jacehensley@gmail.com>
2020-02-20 12:39:45 -0800

    Add tests for claim-kit

---
+++ packages/claim-kit/test/elements/renderClaimQRCode.test.ts
@@ -0,0 +1,45 @@
+import {Options as QROptions} from '@bloomprotocol/qr'
+
+import {renderClaimQRCode} from '../../src/elements/renderClaimQRCode'
+import {ClaimData, ButtonOptions} from '../../src/types'
+
+describe('renderClaimQRCode', () => {
+  let claimQRCode: {
+    update: (config: {claimData: ClaimData; buttonOptions: ButtonOptions; options?: Partial<QROptions>}) => void
+    remove: () => void
+  }
+  let container: HTMLDivElement
+
+  beforeAll(() => {
+    container = document.createElement('div')
+  })
+
+  afterAll(() => {
+    container.remove()
+  })
+
+  beforeEach(() => {
+    claimQRCode = renderClaimQRCode({
+      container,
+      claimData: {
+        version: 1,
+        url: 'https://claim-kit.bloom.co/api/claim',
+        token: 'token',
+      },
+    })
+  })
+
+  afterEach(() => {
+    claimQRCode.remove()
+  })
+
+  test('renders the qr code', () => {
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+
+  test('removes qr code', () => {
+    claimQRCode.remove()
+
+    expect(container.querySelector('canvas')).toBeNull()
+  })
+})
