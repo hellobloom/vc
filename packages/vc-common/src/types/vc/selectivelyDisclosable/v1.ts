8384eec3d39fcd2417b52bd20d579ce160c3f665
djvs <djvansch@gmail.com>
2020-03-17 12:47:35 -0400

    tweaks

--- packages/vc-common/src/types/vc/selectivelyDisclosable/v1.ts
+++ packages/vc-common/src/types/vc/selectivelyDisclosable/v1.ts
@@ -1,4 +1,4 @@
-import {SimpleThing} from '../base/v1'
+import {SimpleThing, TContext} from '../base/v1'
 import {VCV1, VCV1Subject, VCV1Type} from '../v1'
 
 /////////////////////////////////////////////////
@@ -43,8 +43,8 @@ export type SelectiveNodePropertyList = {
   '@properties': Array<string>
 }
 
-export type SelectivePartial = {
-  // @type should be set to "SelectivePartial" if the @type of the origin node is to be omitted - otherwise, string values should be assumed to be the origin node @type.
+export type SelectiveProperty = {
+  // @type should be set to "SelectiveProperty" if the @type of the origin node is to be omitted - otherwise, string values should be assumed to be the origin node @type.
   '@type': string
   // Original VC ID
   '@vcId': string
@@ -54,12 +54,14 @@ export type SelectivePartial = {
   [k: string]: string | number | boolean | undefined | null // Only scalar properties
 }
 
-export type SelectiveType = {
-  '@type': 'SelectiveType'
+export type SelectiveMeta = {
+  '@type': 'SelectiveMeta'
   // Original VC ID
   '@vcId': string
-  // Top-level "type" array of original VC
-  type: VCV1Type
+  // Top-level "type" of original VC
+  '@vcType': VCV1Type
+  // Top-level "@context" of original VC
+  '@vcContext'?: TContext
 }
 
 /////////////////////////////////////////////////
@@ -67,7 +69,7 @@ export type SelectiveType = {
 /////////////////////////////////////////////////
 export type SelectiveNodeWrapper = {nodes: [SelectiveNode]}
 export type SelectiveEdgeWrapper = {edges: [SelectiveEdge]}
-export type VCV1SelectiveStructuralPartialSubjectData = {
+export type VCV1SelectiveStructuralAtomSubjectData = {
   // Original VC ID
   '@vcId': string
   '@type': 'SelectiveStructure'
@@ -89,28 +91,28 @@ export type VCV1SelectiveFullSubject<T extends SimpleThing> = VCV1Subject<T>
 export type VCV1SelectiveStructuralFullType = [VCV1Type[0], 'SelectiveStructuralFullCredential', ...string[]]
 export type VCV1SelectiveStructuralFullSubject = VCV1Subject<VCV1SelectiveStructuralFullSubjectData>
 
-// SelectiveStructuralPartial: Metadata of single node or edge
-export type VCV1SelectiveStructuralPartialType = [VCV1Type[0], 'SelectiveStructuralPartialCredential', ...string[]]
-export type VCV1SelectiveStructuralPartialSubject = VCV1Subject<VCV1SelectiveStructuralPartialSubjectData>
+// SelectiveStructuralAtom: Metadata of single node or edge
+export type VCV1SelectiveStructuralAtomType = [VCV1Type[0], 'SelectiveStructuralAtomCredential', ...string[]]
+export type VCV1SelectiveStructuralAtomSubject = VCV1Subject<VCV1SelectiveStructuralAtomSubjectData>
 
 // SelectiveNodePropertyList: Manifest of properties for node
 export type VCV1SelectiveNodePropertyListType = [VCV1Type[0], 'SelectiveNodePropertyListCredential', ...string[]]
 export type VCV1SelectiveNodePropertyListSubject = VCV1Subject<SelectiveNodePropertyList>
 
-// SelectivePartialSubject: VC for single property of node
-export type VCV1SelectivePartialType = [VCV1Type[0], 'SelectivePartialCredential', ...string[]]
-export type VCV1SelectivePartialSubject = VCV1Subject<SelectivePartial>
+// SelectivePropertySubject: VC for single property of node
+export type VCV1SelectivePropertyType = [VCV1Type[0], 'SelectivePropertyCredential', ...string[]]
+export type VCV1SelectivePropertySubject = VCV1Subject<SelectiveProperty>
 
 // SelectiveTypeSubject: VC for single property of node
-export type VCV1SelectiveTypeType = [VCV1Type[0], 'SelectiveTypeCredential', ...string[]]
-export type VCV1SelectiveTypeSubject = VCV1Subject<SelectiveType>
+export type VCV1SelectiveMetaType = [VCV1Type[0], 'SelectiveMetaCredential', ...string[]]
+export type VCV1SelectiveMetaSubject = VCV1Subject<SelectiveMeta>
 
 /////////////////////////////////////////////////
 // Fully constructed VCs
 /////////////////////////////////////////////////
 export type VCV1SelectiveFull<T extends SimpleThing> = VCV1<VCV1SelectiveFullSubject<T>, VCV1SelectiveFullType>
 export type VCV1SelectiveStructuralFull = VCV1<VCV1SelectiveStructuralFullSubject, VCV1SelectiveStructuralFullType>
-export type VCV1SelectiveStructuralPartial = VCV1<VCV1SelectiveStructuralPartialSubject, VCV1SelectiveStructuralPartialType>
+export type VCV1SelectiveStructuralAtom = VCV1<VCV1SelectiveStructuralAtomSubject, VCV1SelectiveStructuralAtomType>
 export type VCV1SelectiveNodePropertyList = VCV1<VCV1SelectiveNodePropertyListSubject, VCV1SelectiveNodePropertyListType>
-export type VCV1SelectivePartial = VCV1<VCV1SelectivePartialSubject, VCV1SelectivePartialType>
-export type VCV1SelectiveType = VCV1<VCV1SelectiveTypeSubject, VCV1SelectiveTypeType>
+export type VCV1SelectiveProperty = VCV1<VCV1SelectivePropertySubject, VCV1SelectivePropertyType>
+export type VCV1SelectiveMeta = VCV1<VCV1SelectiveMetaSubject, VCV1SelectiveMetaType>
