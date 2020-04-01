3dad4598c74ab2043e2aa03de15a58c09388494d
Jace <jacehensley@gmail.com>
2020-04-01 15:30:42 -0700

    Improve docs landing page

--- packages/guidelines/docs/docusaurus.config.js
+++ packages/guidelines/docs/docusaurus.config.js
@@ -124,9 +124,10 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.scss'),
         },
       },
     ],
   ],
+  plugins: ['docusaurus-plugin-sass'],
 }
