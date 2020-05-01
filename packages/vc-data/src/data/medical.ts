cb56d5ecfe19511390539d56b370c321a71defb1
djvs <djvansch@gmail.com>
2020-05-01 11:46:02 -0400

    add support for FHIR VICs

--- packages/vc-data/src/data/medical.ts
+++ packages/vc-data/src/data/medical.ts
@@ -1,11 +1,49 @@
 import {VCV1} from '@bloomprotocol/vc-common'
 import {Subject, MaybeArray, Credential, GovernmentOrg} from './base'
 import {Person, Organization, MedicalClinic, QuantitativeValue, Drug, DefinedTerm} from 'schema-dts'
+import {R4} from '@ahryman40k/ts-fhir-types'
 
 //////////////////////////////////////////////////////////////
 // Medical test VCs
 //////////////////////////////////////////////////////////////
 
+// FHIR types
+export interface IImmunization extends R4.IImmunization {
+  resourceType: 'Immunization'
+  id: string
+  status: string
+  vaccineCode: R4.ICodeableConcept
+  patient: R4.IReference
+  encounter: R4.IReference
+  occurrenceDateTime: string
+  primarySource: boolean
+  location: R4.IReference
+  manufacturer: R4.IReference
+  lotNumber: string
+  site: R4.ICodeableConcept
+  route: R4.ICodeableConcept
+  doseQuantity: R4.IQuantity
+}
+export interface IObservation extends R4.IObservation {
+  resourceType: 'Observation'
+  category: Array<R4.ICodeableConcept>
+  identifier: Array<R4.IIdentifier>
+  status: R4.ObservationStatusKind
+  method: R4.ICodeableConcept
+  device: R4.IReference
+  code: R4.ICodeableConcept
+  subject: R4.IReference
+  effectiveDateTime: string
+  performer: Array<R4.IReference>
+  component: Array<R4.IObservation_Component>
+}
+export type VCSMedicalFHIRImmunization = Subject<IImmunization>
+export type VCSMedicalFHIRObservation = Subject<IObservation>
+
+export type VCMedicalFHIRImmunization = VCV1<VCSMedicalFHIRImmunization>
+export type VCMedicalFHIRObservation = VCV1<VCSMedicalFHIRObservation>
+
+// Schema.org-based types
 export type MedicalClinicE = MedicalClinic & {
   name?: string
   identifier?: string | number
